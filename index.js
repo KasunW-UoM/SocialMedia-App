c753a3602d7aa74cf15aa238a0d43792706e2163
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2021-06-10 01:55:11 +0530

    create user.js and posts.js file

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-
+type Post{
+    id:ID!
+    body:String!
+    createat:String!
+    username:String!
+}
   type Query {
-    sayHi: String!
+    getPosts:[Post]
   }
 `;
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello world'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+       }
     }
 }
 
