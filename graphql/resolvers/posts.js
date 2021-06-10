f5aa308ba83936c2f82afcba28d36b4055f1eae9
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2021-06-11 01:08:54 +0530

    login and registration part

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,15 @@
+
+const Post = require('../../models/Post');
+
+module.exports = {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find().sort({ createdAt: -1 });
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    }
+};
---
+++ graphql/typeDefs.js
@@ -0,0 +1,32 @@
+const { gql } = require('apollo-server');
+
+
+module.exports = gql`
+
+type Post{
+    id:ID!
+    body:String!
+    createat:String!
+    username:String!
+}
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  
+input RegisterInput{
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+}
+  type Query {
+    getPosts:[Post]
+  }
+  type Mutation{
+      register(registerInput: RegisterInput) :User!
+  }
+`;
