7a1bd3fdad88acbaaa8604c39aa46e8f3ccca5a9
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2021-06-14 13:41:37 +0530

    Authentication create

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -25,9 +25,12 @@ input RegisterInput{
 }
   type Query {
     getPosts:[Post]
+    getPost(postId:ID!):Post
   }
   type Mutation{
       register(registerInput: RegisterInput) :User!
       login(username:String!,password:String!):User!
+      createPost(body:String!):Post!
+       deletePost(postId:ID!):String!
   }
 `;
